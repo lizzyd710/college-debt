8bff54dd66c6d2c7802fe9df7b00bdffcad08ebc
Elizabeth Sherrock <lizzyd710@gmail.com>
2019-12-19 18:26:48 -0500

    Takes input for indicator ID

--- src/college_debt.py
+++ src/college_debt.py
@@ -1,5 +1,9 @@
 import wbdata as wb
 import pandas as p
 
+# Temporary way of keeping track of the debt amount.
+DEBT = 1.5 * 10 ** 12
+
 if __name__ == '__main__':
-    print("There will be something here. Eventually.")
+    id = input("Enter indicator ID: ")
+    indicator = wb.get_indicator(id)
